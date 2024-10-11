7bb0ebf4af4e657692824ca6da85dfc121e3c287
Vasilisa Shelkova <100698142+shelkovichka@users.noreply.github.com>
2024-10-11 16:35:59 +0300

    styles fixed

--- src/shared/TextField.tsx
+++ src/shared/TextField.tsx
@@ -1,6 +1,6 @@
 import { TextField } from '@material-ui/core';
 import { IFormInputProps } from '../features/CreateProfile/FormViews.types';
-import { Controller, ControllerRenderProps } from "react-hook-form";
+import { Controller } from "react-hook-form";
 
 const RegisterInput: React.FC<IFormInputProps> = ({
   name,
@@ -30,66 +30,3 @@ const RegisterInput: React.FC<IFormInputProps> = ({
 
 
 export default RegisterInput;
-
-
-// const RegisterInput = () => {
-//     const { control } = useFormContext();
-
-//     return (
-//         <>
-//             <div>
-//                 <Controller 
-//                     name="name"
-//                     defaultValue=""
-//                     rules={{required: true}}
-//                     control={control}
-//                     render={({field}) => (
-//                         <TextField
-//                             {...field} 
-//                             placeholder="Name" 
-//                             type="text"
-//                             variant="outlined"
-//                             color="primary"
-//                         />
-//                     )}
-//                 />
-//             </div>
-//             <div>
-//                 <Controller 
-//                     name="email"
-//                     defaultValue=""
-//                     rules={{required: true}}
-//                     control={control}
-//                     render={({field}) => (
-//                         <TextField
-//                             {...field} 
-//                             placeholder="Email" 
-//                             type="email"
-//                             variant="outlined" 
-//                             color="primary"
-//                         />
-//                     )}
-//                 />
-//             </div>
-//             <div>
-//                 <Controller 
-//                     name="password"
-//                     defaultValue=""
-//                     rules={{required: true}}
-//                     control={control}
-//                     render={({field}) => (
-//                         <TextField
-//                             {...field} 
-//                             placeholder="Password" 
-//                             type="password"
-//                             variant="outlined" 
-//                             color="primary"
-//                         />
-//                     )}
-//                 />
-//             </div>
-//         </>
-//     )
-// }
-
-// export default RegisterInput;
